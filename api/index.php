e1075151979652b237dea0e6b817e0c5858d6f2d
PieroL1 <198247247+PieroL1@users.noreply.github.com>
2025-11-03 17:34:27 -0500

    Update print statement from 'Hello' to 'Goodbye'color

--- api/index.php
+++ api/index.php
@@ -54,7 +54,7 @@
         include("secciones/portada.php");
       ?>
 
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-primary">
         <div class="col-md-12 px-0">
           <h1 class="display-4 font-italic">
             <?php
@@ -626,4 +626,5 @@
     </script>
   </body>
 </html>
+
 
